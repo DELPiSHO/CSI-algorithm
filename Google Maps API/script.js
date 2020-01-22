68c82dd85a0f643859fc97325a66f55b08779941
Yauheni Dzianisau <zhekadeni98@mail.ru>
2020-01-22 22:50:02 +0100

    google maps api beginning

---
+++ Google Maps API/script.js
@@ -0,0 +1,68 @@
+// Load the Visualization API and the columnchart package.
+google.load('visualization', '1', {packages: ['columnchart']});
+
+function initMap() {
+  // The following path marks a path from Mt. Whitney, the highest point in the
+  // continental United States to Badwater, Death Valley, the lowest point.
+  var path = [
+      {lat: 53.77, lng: 20.39}, // Olsztyn
+      {lat: 54.44, lng: 18.53}  // Sopot
+      ];
+
+  var elevator = new google.maps.ElevationService();
+
+  displayPathElevation(path, elevator);
+}
+
+function displayPathElevation(path, elevator) {
+
+  elevator.getElevationAlongPath({
+    'path': path,
+    'samples': 256
+  }, plotElevation);
+}
+
+function download(elevations) {
+
+		var headers = ['lat', 'lng', 'elevation']
+    
+    var  heights = [];
+    var latlng = [];
+    var lng = [];
+    
+    var length = 0;
+    
+		let csvContent = "data:text/csv;charset=utf-8,"; 
+    
+		for (var elevationObject of elevations) {
+  		heights.push(elevationObject.elevation);
+      latlng.push(elevationObject.location);
+      length += 1;
+    }
+    
+    var data = [latlng, heights]
+    
+    
+		let row = headers.join(",");         
+    csvContent += row + "\r\n";
+        
+    for (i = 0; i < length; i++) {
+          csvContent += latlng[i]  + ',' + heights[i] + '\r\n';
+    }
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "olsztyn-sopot.csv");
+    document.body.appendChild(link);
+
+    link.click();
+}
+
+function plotElevation(elevations, status) {
+
+	var trasa1 = document.getElementById('dane_pierwsza_trasa');
+
+  download(elevations);
+
+}
